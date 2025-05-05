2505820e6ab7934abcf8bdab935bbd305cf96baa
Atchutha Rao T A <atchutha57@gmail.com>
2025-05-05 12:19:02 +0530

    Add login and signup pages; update root page layout and links

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import Link from "next/link";
 
-const Rootpage = () => {
+const RootPage = () => {
   return (
     <div className="h-svh container mx-auto">
-      <div className="h-full flex flex-col items-center justify-center gap-2">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl ">
-          platinum
-        </h1>
-        <p>platinum is the world&apos;s largest tech news aggregator</p>
+      <div className="h-full flex flex-col items-center justify-center gap-6">
+        <div className="flex flex-col items-center justify-center gap-0.5">
+          <h1 className="text-4xl font-extrabold text-balance max-w-prose">
+            Platinum
+          </h1>
+          <p className="max-w-prose">
+            Platinum is the world&apos;s largest tech news aggregator!
+          </p>
+        </div>
+
         <div className="flex flex-row items-center gap-4">
-          <Button>Sign Up</Button>
-          <Button variant="secondary">Log In</Button>
+          <Button asChild>
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/log-in">Log in</Link>
+          </Button>
         </div>
       </div>
     </div>
   );
 };
 
-export default Rootpage;
+export default RootPage;
